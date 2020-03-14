60ffc410940b2cafbc1fa933f7d88c24dea0ecd8
折田和啓 <orikazu0824@oritawakeinoMacBook-puro.local>
2020-03-14 19:33:18 +0900

    xpath-test

--- dom-test.php
+++ dom-test.php
@@ -1,1 +1,20 @@
 <?php
+
+$dom = new DOMDocument;
+$xpath = new DOMXPath($dom);
+
+
+$root = $dom->createElement('root');
+$dom->appendChild($root);
+
+$test = $dom->createElement('test', 'test_value');
+$root->appendChild($test);
+$test->setAttribute('class', 'test_class');
+
+$test_node = $xpath->query('//*[@class="test_class"]')->item(0);
+$test2 = $dom->createElement('test2', $test_node->nodeValue);
+$root->appendChild($test2);
+
+echo $dom->saveXML();
+
+//var_dump($test_node->nodeValue);
